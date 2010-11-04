f7e12d4ec5d2eded3e6a838d441664697ac96dfd
Robert Hunt <Hunt.RobertJ@gmail.com>
2010-11-04 20:43:13 +1300

    BookCodes completed, progress on versification systems

---
+++ runAll.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# runAll.sh
+#
+#   Last modified: 2010-11-03 RJH
+#
+# Run xmllint on all the XML files in the DataFiles folder
+# Then run Python code for each module
+#
+# Run xmllint on all the XML files in the DataFiles folder
+sh checkAll.sh
+
+echo "Running Python modules..."
+
+python3 iso_639_3.py
+python3 BibleBooksCodes.py
+
+echo "Testing .py table files..."
+python3 DerivedFiles/iso_639_3Tables.py
+python3 DerivedFiles/BibleBooksCodesTables.py
+
+echo "Testing compilation of .h files..."
+cc include_test.c
